5c4353ad2be03357139758ae2c780b7b45266147
Marek <mar.jan@int.pl>
2020-11-07 22:20:33 +0100

    init repo

---
+++ src/main/java/com/mj/wims/WimsApplication.java
@@ -0,0 +1,13 @@
+package com.mj.wims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj/wims/WimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.wims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
