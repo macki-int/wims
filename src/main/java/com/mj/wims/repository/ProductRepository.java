da277eedd12e99cfdda3fe8fd528bc5c716ef423
macki-int <macki@int.pl>
2020-11-18 16:07:00 +0100

    add query findAllProductAndInventoryByProductTypeId in ProductRepository

--- src/main/java/com/mj/wims/repository/ProductRepository.java
+++ src/main/java/com/mj/wims/repository/ProductRepository.java
@@ -12,7 +12,10 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<String> findByName(String name);
 
+    @Query(value = "SELECT product.name, product.id, inventory.product_id FROM product INNER JOIN inventory ON product.id = inventory.product_id WHERE  product_type_id = ?1", nativeQuery = true)
+
 //    @Query("SELECT p FROM Products p WHERE p.product_type_id = ?")
-//    @Query("SELECT p, i FROM Product p, Inventory i INNER JOIN p ON p.id = i.product_id WHERE p.product_type_id = ?")
+//    @Query("SELECT p, i FROM Product p, Inventory i INNER JOIN p ON p.id = i.product_id WHERE p.product_type_id = :productTypeId")
+//    @Query("SELECT p, i FROM Product p JOIN p.Inventory i WHERE p.product_type_id = ?1")
     List<Object> findAllByProductTypeId(Long productTypeId);
 }
