d58d6613099c34421f4ebb4e60c0625b8dfa2f8e
macki <macki@int.pl>
2021-01-17 20:26:21 +0100

    add method onAuthenticationSuccessHandler

--- src/main/java/com/mj/wims/config/RestAuthenticationSuccessHandler.java
+++ src/main/java/com/mj/wims/config/RestAuthenticationSuccessHandler.java
@@ -1,4 +1,37 @@
 package com.mj.wims.config;
 
-public class RestAuthenticationSuccessHandler {
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private long expirationTime;
+    private String secret;
+
+    public RestAuthenticationSuccessHandler(
+            @Value("${jwt.expirationTime}") long expirationTime,
+            @Value("${jwt.secret}") String secret) {
+        this.expirationTime = expirationTime;
+        this.secret = secret;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.addHeader("Authorization", "Bearer " + token);
+    }
 }
