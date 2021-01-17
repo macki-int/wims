2d970bb53e6650bcfac67a3cdcb4ad84f43c7670
macki <macki@int.pl>
2021-01-17 19:41:54 +0100

    add LoginCredentials

---
+++ src/main/java/com/mj/wims/model/LoginCredentials.java
@@ -0,0 +1,17 @@
+package com.mj.wims.model;
+
+public class LoginCredentials {
+    private String username;
+    private String password;
+
+    public LoginCredentials() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
