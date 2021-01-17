b7ba9d5e1c7c2d6b44e7835dd2de71d8248ebb62
macki-int <macki@int.pl>
2021-01-18 00:14:46 +0100

    add WebSecurityConfig

---
+++ src/main/java/com/mj/wims/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.mj.wims.config;
+
+import com.mj.wims.model.User;
+import com.mj.wims.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/style/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+//                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .csrf().disable();
+
+//        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optionalUser = userRepository.findByUsername(username);
+                return (UserDetails) optionalUser.get();
+            }
+        }).passwordEncoder(passwordEncoder());
+    }
+}
