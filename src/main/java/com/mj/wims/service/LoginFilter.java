516d7ec33c7f80913a4a631899c2f03956d40fa3
macki-int <macki@int.pl>
2021-01-21 00:04:54 +0100

    add filter LoginFilter

---
+++ src/main/java/com/mj/wims/service/LoginFilter.java
@@ -0,0 +1,53 @@
+package com.mj.wims.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mj.wims.model.UserCredentials;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public LoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    protected LoginFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse) throws AuthenticationException, IOException {
+        UserCredentials userCredentials = new ObjectMapper()
+            .readValue(httpServletRequest.getInputStream(), UserCredentials.class);
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userCredentials.getUserName(),
+                        userCredentials.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse, FilterChain chain,
+            Authentication auth) {
+        AuthenticationService.addJWTToken(httpServletResponse, auth.getName());
+    }
+}
