7c4dc6f6f1381c8fcdde2556927b819630c8c83f
macki-int <macki@int.pl>
2021-01-20 23:27:12 +0100

    add autentictionService

--- src/main/java/com/mj/wims/config/WebSecurityConfig.java
+++ src/main/java/com/mj/wims/config/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package com.mj.wims.config;
 
-import com.mj.wims.model.User;
 import com.mj.wims.repository.UserRepository;
+import com.mj.wims.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,40 +15,41 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-//                .antMatchers("/*").permitAll()
-//                .antMatchers("/login").permitAll()
-                .antMatchers("/*").permitAll()
-//                .antMatchers("/*").hasRole("ADMIN")
-                .antMatchers("/swagger-ui/").permitAll()
-                .antMatchers("/v2/api-docs").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/swagger-resources/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-//                .formLogin()
-//                .loginPage("/users/login").permitAll()
-//                .and()
-//                .logout().permitAll()
-//                .and()
-                .csrf().disable();
-
-//        http.authorizeRequests().antMatchers("/**").permitAll();
-    }
-
     @Autowired
-    UserRepository userRepository;
+    private UserDetailsServiceImpl customUserDetailsService;
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+////                .antMatchers("/*").permitAll()
+////                .antMatchers("/login").permitAll()
+//                .antMatchers("/*").permitAll()
+////                .antMatchers("/*").hasRole("ADMIN")
+//                .antMatchers("/swagger-ui/").permitAll()
+//                .antMatchers("/v2/api-docs").permitAll()
+//                .antMatchers("/webjars/**").permitAll()
+//                .antMatchers("/swagger-resources/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+////                .formLogin()
+////                .loginPage("/users/login").permitAll()
+////                .and()
+////                .logout().permitAll()
+////                .and()
+//                .csrf().disable();
+//
+////        http.authorizeRequests().antMatchers("/**").permitAll();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -55,13 +57,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findUserByUsername(username);
-            }
-        });
-//        }).passwordEncoder(passwordEncoder());
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new AuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    //    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList("*"));
+//        config.setAllowedMethods(Arrays.asList("*"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowCredentials(true);
+//        config.applyPermitDefaultValues();
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(customUserDetailsService);
     }
 }
---
+++ src/main/java/com/mj/wims/service/AuthenticationService.java
@@ -0,0 +1,4 @@
+package com.mj.wims.service;
+
+public class AuthenticationService {
+}
