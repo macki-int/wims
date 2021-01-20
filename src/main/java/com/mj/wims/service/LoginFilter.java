d0480cd7053e75197f665f5792ce3a3cfb5901bc
macki-int <macki@int.pl>
2021-01-21 00:41:22 +0100

    remove import ServletException

--- src/main/java/com/mj/wims/service/LoginFilter.java
+++ src/main/java/com/mj/wims/service/LoginFilter.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
