18beb85cdbbe7b413212d89f25a5e7a80cd039d6
macki-int <macki@int.pl>
2021-01-31 20:58:40 +0100

    add LoginDTO class

---
+++ src/main/java/com/mj/wims/dto/PasswordDTO.java
@@ -0,0 +1,16 @@
+package com.mj.wims.dto;
+
+public class PasswordDTO {
+    private String password;
+
+    public PasswordDTO() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
