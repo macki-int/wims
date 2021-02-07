3f955fde235623493341df324d9009aa84073f1c
macki-int <macki@int.pl>
2021-02-07 09:17:55 +0100

    add test InventoryController

---
+++ src/test/java/com/mj/wims/InventoryControllerTest.java
@@ -0,0 +1,23 @@
+package com.mj.wims;
+
+import com.mj.wims.controller.InventoryController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class InventoryControllerTest {
+
+    @Autowired
+    private InventoryController inventoryController;
+
+    @Test
+    public void userControllerInitializedCorrectly(){
+        assertThat(inventoryController).isNotNull();
+    }
+}
