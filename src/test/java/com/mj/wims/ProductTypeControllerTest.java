d682cfb680f3b73666a484627dcec6bb6d33b73e
macki-int <macki@int.pl>
2021-02-07 09:19:13 +0100

    add test ProductTypeController

---
+++ src/test/java/com/mj/wims/ProductTypeControllerTest.java
@@ -0,0 +1,23 @@
+package com.mj.wims;
+
+import com.mj.wims.controller.ProductTypeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ProductTypeControllerTest {
+
+    @Autowired
+    private ProductTypeController productTypeController;
+
+    @Test
+    public void userControllerInitializedCorrectly(){
+        assertThat(productTypeController).isNotNull();
+    }
+}
