67301828d7930f7af116fd16e0abb924aa886083
macki-int <macki@int.pl>
2021-02-25 13:53:44 +0100

    add field oldPassword in PasswordDTO.class

--- src/main/java/com/mj/wims/dto/PasswordDTO.java
+++ src/main/java/com/mj/wims/dto/PasswordDTO.java
@@ -2,6 +2,7 @@ package com.mj.wims.dto;
 
 public class PasswordDTO {
     private String password;
+    private String oldPassword;
 
     public PasswordDTO() {
     }
@@ -12,5 +13,13 @@ public class PasswordDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
     }
 }
