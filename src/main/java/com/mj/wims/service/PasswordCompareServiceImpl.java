071d078575b1c2e8151c819a5378ecae52824350
macki-int <macki@int.pl>
2021-02-25 21:19:55 +0100

    add BCrypt check password

--- src/main/java/com/mj/wims/service/PasswordCompareServiceImpl.java
+++ src/main/java/com/mj/wims/service/PasswordCompareServiceImpl.java
@@ -1,7 +1,9 @@
 package com.mj.wims.service;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class PasswordCompareServiceImpl implements PasswordCompareService {
     public boolean comparePassword(String actualPassword, String oldPassword){
-        return actualPassword.equals(oldPassword);
+        return BCrypt.checkpw(oldPassword, actualPassword);
     }
 }
