dc8a4552e721f6813a4dbc1102cdd446a1b985cf
Marek <macki@int.pl>
2021-03-03 22:29:09 +0100

    add UserNameWithoutPasswordDTO clas

---
+++ src/main/java/com/mj/wims/dto/UserNameWithoutPasswordDTO.java
@@ -0,0 +1,54 @@
+package com.mj.wims.dto;
+
+import com.mj.wims.model.RoleEnum;
+
+public class UserNameWithoutPasswordDTO {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private RoleEnum role;
+    private Boolean active;
+
+    public UserNameWithoutPasswordDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
