4c7d78e2d1eb8eaf2272edfe81fcff911a5c9f4b
Marek <macki@int.pl>
2021-03-12 00:59:19 +0100

    temp ProductRepository HQL query

--- src/main/java/com/mj/wims/repository/ProductRepository.java
+++ src/main/java/com/mj/wims/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedNativeQuery;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,15 +25,19 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 //    Select i from Inventory i,Category c INNER JOIN i.product ip INNER JOIN c.products cp where ip = cp and c.id=?
     //select * from products join inventories on inventories.product_id = products.id left join reservations on reservations.inventory_id = inventories.id
 //    @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
-    @Query("SELECT r FROM Reservation r LEFT JOIN r.inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
+//    @Query("SELECT r FROM Reservation r JOIN r.inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
+    //withInactiveValue == false and withZeroValue == true
+
+    @Query("SELECT r FROM Reservation r ")
     List<Object> findAllProductsAndZeroQuantityByProductTypeId(Long productTypeId);
 
-    //withInactiveValue == false and withZeroValue == true
     @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1  AND p.active = true ORDER BY p.id ASC")
     List<Object> findActiveProductsAndZeroQuantityByProductTypeId(Long productTypeId);
 
     //withInactiveValue == true and withZeroValue == false
-    @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
+    @Query("SELECT i, COUNT(Reservation.inventory) FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
+//SELECT COUNT(r.inventory) FROM Reservation r
+//    @Query("SELECT i, r FROM Reservation r RIGHT JOIN r.inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
     List<Object> findActiveProductsAndNotZeroQuantityByProductTypeId(Long productTypeId);
 
     //withInactiveValue == false and withZeroValue == false
