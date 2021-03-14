e10852940398272ae995607a11b648a7c0a4bef8
Marek <macki@int.pl>
2021-03-14 09:14:50 +0100

    change hql query for product - next version

--- src/main/java/com/mj/wims/repository/ProductRepository.java
+++ src/main/java/com/mj/wims/repository/ProductRepository.java
@@ -23,20 +23,20 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     //withInactiveValue == true and withZeroValue == true
 //    Select i from Inventory i,Category c INNER JOIN i.product ip INNER JOIN c.products cp where ip = cp and c.id=?
-    //select * from products join inventories on inventories.product_id = products.id left join reservations on reservations.inventory_id = inventories.id
-//    @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
 //    @Query("SELECT r FROM Reservation r JOIN r.inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
-    //withInactiveValue == false and withZeroValue == true
+    //withInactiveValue == true and withZeroValue == false
 
-    @Query("SELECT r FROM Reservation r ")
+    @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
     List<Object> findAllProductsAndZeroQuantityByProductTypeId(Long productTypeId);
 
+    //withInactiveValue == false and withZeroValue == true
     @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1  AND p.active = true ORDER BY p.id ASC")
     List<Object> findActiveProductsAndZeroQuantityByProductTypeId(Long productTypeId);
 
-    //withInactiveValue == true and withZeroValue == false
-    @Query("SELECT i, COUNT(Reservation.inventory) FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
-//SELECT COUNT(r.inventory) FROM Reservation r
+    //    select *, (SELECT COUNT(*) FROM reservations WHERE reservations.inventory_id=inventories.id) from products join inventories on inventories.product_id = products.id;
+//        @Query("SELECT i FROM Inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
+    @Query("SELECT i, (SELECT COUNT(r) FROM Reservation r WHERE r.inventory.id) FROM Inventory i JOIN i.product p WHERE product_type_id = ?1")
+
 //    @Query("SELECT i, r FROM Reservation r RIGHT JOIN r.inventory i JOIN i.product p WHERE product_type_id = ?1 AND p.active = true AND i.quantity > 0 ORDER BY p.id ASC")
     List<Object> findActiveProductsAndNotZeroQuantityByProductTypeId(Long productTypeId);
 
@@ -45,8 +45,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Object> findAllProductsAndNotZeroQuantityByProductTypeId(Long productTypeId);
 
 //    @Query("SELECT p FROM Product p WHERE product_type_id = ?1 ORDER BY p.id ASC")
-
-
     @Query("SELECT MAX(i.updateDate) FROM Inventory i JOIN i.product p WHERE product_type_id = ?1")
     List<Object> findMaxUpdateDateByProductType(Long productTypeId);
 }
