8323f3930e44eac80964b384608108a907947ed3
Marek <macki@int.pl>
2021-03-16 00:21:01 +0100

    add InventoryWithReservationCounterDTO

--- src/main/java/com/mj/wims/dto/InventoryWithReservationCounterDTO.java
+++ src/main/java/com/mj/wims/dto/InventoryWithReservationCounterDTO.java
@@ -1,16 +1,17 @@
-package com.mj.wims.model;
+package com.mj.wims.dto;
 
-public class ProductInventoryReservation {
+import com.mj.wims.model.Inventory;
+import com.mj.wims.model.Product;
+
+public class InventoryWithReservationCounterDTO {
     private Inventory inventory;
-    private Product product;
     private Long reservationCounter;
 
-    public ProductInventoryReservation() {
+    public InventoryWithReservationCounterDTO() {
     }
 
-    public ProductInventoryReservation(Inventory inventory, Product product, Long reservationCounter) {
+    public InventoryWithReservationCounterDTO(Inventory inventory, Long reservationCounter) {
         this.inventory = inventory;
-        this.product = product;
         this.reservationCounter = reservationCounter;
     }
 
@@ -20,14 +21,6 @@ public class ProductInventoryReservation {
 
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
     }
 
     public Long getReservationCounter() {
