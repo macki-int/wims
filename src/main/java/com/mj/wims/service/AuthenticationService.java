435a476e8ffbadbabc9a55923a857501accc219e
Marek <macki@int.pl>
2021-03-21 21:40:36 +0100

    add role to JWT token

--- src/main/java/com/mj/wims/service/AuthenticationService.java
+++ src/main/java/com/mj/wims/service/AuthenticationService.java
@@ -2,26 +2,27 @@ package com.mj.wims.service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Date;
 
-import static java.util.Collections.emptyList;
 
 
 public class AuthenticationService {
-//    @Value("${jwt.expirationTime}")
+    //    @Value("${jwt.expirationTime}")
     private static final long EXPIRATIONTIME = 14_400_000L;
 
-//    @Value("${jwt.secretKey}")
+    //    @Value("${jwt.secretKey}")
     private static final String SIGNINGKEY = "asffgfg";
 
-    static public void addJWTToken(HttpServletResponse response, String username) {
+    static public void addJWTToken(HttpServletResponse response, String username, SimpleGrantedAuthority role ) {
         String JwtToken = Jwts.builder().setSubject(username)
+                .claim("roles", role.getAuthority())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
                 .signWith(SignatureAlgorithm.HS512, SIGNINGKEY)
                 .compact();
@@ -38,8 +39,16 @@ public class AuthenticationService {
                     .getBody()
                     .getSubject();
 
+            String userRole = Jwts.parser()
+                    .setSigningKey(SIGNINGKEY)
+                    .parseClaimsJws(token.replace("Bearer ", ""))
+                    .getBody()
+                    .get("roles")
+                    .toString();
+
             if (user != null) {
-                return new UsernamePasswordAuthenticationToken(user, null, emptyList());
+                //emptyList()
+                return new UsernamePasswordAuthenticationToken(user, null, Arrays.asList (new SimpleGrantedAuthority(userRole)));
             } else {
                 throw new RuntimeException("Authentication failed");
             }
