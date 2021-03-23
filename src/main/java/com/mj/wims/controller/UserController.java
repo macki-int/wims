f3d616388ca6eb66e43e98de0f7b2a645d88af17
Marek <macki@int.pl>
2021-03-23 01:06:25 +0100

    remove unused imports

--- src/main/java/com/mj/wims/controller/UserController.java
+++ src/main/java/com/mj/wims/controller/UserController.java
@@ -14,9 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
