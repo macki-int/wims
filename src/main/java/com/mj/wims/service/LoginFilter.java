8afcfdd612e157af2aa7fadff5c7c2e3daf20f3c
Marek <macki@int.pl>
2021-03-23 01:05:23 +0100

    remove unused imports

--- src/main/java/com/mj/wims/service/LoginFilter.java
+++ src/main/java/com/mj/wims/service/LoginFilter.java
@@ -15,7 +15,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
