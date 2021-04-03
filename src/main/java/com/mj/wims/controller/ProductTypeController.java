9d8e8a2c84d658a853c027a3d4932516ba02aa75
macki-int <macki@int.pl>
2021-04-03 10:42:05 +0200

    remove unuse import sprirng security

--- src/main/java/com/mj/wims/controller/ProductTypeController.java
+++ src/main/java/com/mj/wims/controller/ProductTypeController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
