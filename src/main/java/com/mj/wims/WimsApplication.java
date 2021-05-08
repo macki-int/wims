6f44adc156f4fa1cb097060a7b48ed53d6298722
Marek <macki@int.pl>
2021-05-09 00:11:54 +0200

    add wims logging

--- src/main/java/com/mj/wims/WimsApplication.java
+++ src/main/java/com/mj/wims/WimsApplication.java
@@ -1,12 +1,17 @@
 package com.mj.wims;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WimsApplication {
+    private static final Logger LOGGER = LogManager.getLogger(WimsApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(WimsApplication.class, args);
+
+        LOGGER.info("Stat app Wims");
     }
 }
