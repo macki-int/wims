2e1073d4a13e871e87433522c3c0c802ec92081f
Marek <macki@int.pl>
2021-05-11 22:39:27 +0200

    fix log message stat -> start

--- src/main/java/com/mj/wims/WimsApplication.java
+++ src/main/java/com/mj/wims/WimsApplication.java
@@ -12,6 +12,6 @@ public class WimsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WimsApplication.class, args);
 
-        LOGGER.info("Stat app Wims");
+        LOGGER.info("Start app Wims");
     }
 }
