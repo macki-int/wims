ca9fa0a7a0095ea04def214d9139eb7729744da5
macki-int <macki@int.pl>
2021-09-09 00:04:50 +0200

    add cors desable

--- src/main/java/com/mj/wims/config/WebSecurityConfig.java
+++ src/main/java/com/mj/wims/config/WebSecurityConfig.java
@@ -52,10 +52,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
                 .cors()
                 .and()
+                .csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users/login").permitAll()
 //                .antMatchers(HttpMethod.PATCH, "/products/**").hasRole("ADMIN")
