93888f58f8341eb6b581ac5ea6816fc375a9cb45
macki-int <macki@int.pl>
2021-09-27 21:46:51 +0200

    change return type

--- src/main/java/com/mj/wims/controller/DeliveryController.java
+++ src/main/java/com/mj/wims/controller/DeliveryController.java
@@ -3,6 +3,7 @@ package com.mj.wims.controller;
 import com.mj.wims.model.Delivery;
 import com.mj.wims.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class DeliveryController {
 
     @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping()
-    public List<Delivery> getAll(){
+    public ResponseEntity<?> getAll(){
         return deliveryRepository.findAll();
     }
 
