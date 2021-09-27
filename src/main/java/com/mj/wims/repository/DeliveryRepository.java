424ffe67a61de60f6877cc728ca4dedee8565b4e
macki-int <macki@int.pl>
2021-09-27 22:11:20 +0200

    add declaration of method getAllBeforeDeliveryDate in repository

--- src/main/java/com/mj/wims/repository/DeliveryRepository.java
+++ src/main/java/com/mj/wims/repository/DeliveryRepository.java
@@ -2,8 +2,14 @@ package com.mj.wims.repository;
 
 import com.mj.wims.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long>{
+    @Query("SELECT d FROM Delivery d JOIN d.inventory i WHERE d.dateOfDelivery < ?1 ORDER BY d.dateOfDelivery ASC")
+    List<Object> findAllBeforeDeliveryDate(LocalDate date);
 }
