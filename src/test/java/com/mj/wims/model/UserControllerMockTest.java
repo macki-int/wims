e1ecb1ba51ea79d07b55fa5845088a110c34be36
macki-int <macki@int.pl>
2021-09-28 23:18:00 +0200

    add test for method findAll in UserController class

---
+++ src/test/java/com/mj/wims/model/UserControllerMockTest.java
@@ -0,0 +1,53 @@
+package com.mj.wims.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mj.wims.controller.UserController;
+import com.mj.wims.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+@WebMvcTest(UserController.class)
+public class UserControllerMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private UserRepository userRepository;
+
+
+    User USER_1 = new User("adamk", "Adam", "Kowalski", "1234", RoleEnum.ROLE_ADMIN, true);
+
+    @Test
+    public void findAllUserTest() throws Exception {
+        List<User> userList = new ArrayList<>(Arrays.asList(USER_1));
+
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
+//https://stackabuse.com/guide-to-unit-testing-spring-boot-rest-apis/
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/users")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[2].name", is("Jane Doe")));
+    }
+
+}
