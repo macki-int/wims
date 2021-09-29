0baa5eae563420bec367450f39313917684c5a84
macki-int <macki@int.pl>
2021-09-29 23:03:51 +0200

    add createDelivery method

--- src/main/java/com/mj/wims/controller/DeliveryController.java
+++ src/main/java/com/mj/wims/controller/DeliveryController.java
@@ -1,7 +1,9 @@
 package com.mj.wims.controller;
 
+import com.mj.wims.model.Delivery;
 import com.mj.wims.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +23,13 @@ public class DeliveryController {
 
     @RolesAllowed("ROLE_ADMIN")
     @GetMapping()
-    public ResponseEntity<?> findAll(){
+    public ResponseEntity<?> findAll() {
         return ResponseEntity.ok().body(deliveryRepository.findAll());
     }
 
     @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/valid-date")
-    public ResponseEntity<?> getAllBeforeDeliverDate(@RequestParam String date){
+    public ResponseEntity<?> getAllBeforeDeliverDate(@RequestParam String date) {
         LocalDate localDate = LocalDate.parse(date);
 
         return ResponseEntity.ok().body(deliveryRepository.findAllBeforeDeliveryDate(localDate));
@@ -35,8 +37,21 @@ public class DeliveryController {
 
     @RolesAllowed({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/inventories/{id}")
-    public ResponseEntity<?> findAllDeliveriesByInventoryId(@PathVariable Long id){
+    public ResponseEntity<?> findAllDeliveriesByInventoryId(@PathVariable Long id) {
         return ResponseEntity.ok().body(deliveryRepository.findAllDeliveriesByInventoryId(id));
+    }
+
+    @RolesAllowed("ROLE_ADMIN")
+    @PostMapping()
+    public ResponseEntity<?> createDelivery(@RequestBody Delivery delivery) {
+
+        try {
+            deliveryRepository.save(delivery);
+            return ResponseEntity.status(HttpStatus.CREATED).body(delivery);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.badRequest().body("Object did not create");
     }
 
 
