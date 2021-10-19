681bb2d3b515ef6f8c99b1a120519945b70f437a
macki-int <macki@int.pl>
2021-10-19 22:42:09 +0200

    add findAll method in MessageController

--- src/main/java/com/mj/wims/controller/MessageController.java
+++ src/main/java/com/mj/wims/controller/MessageController.java
@@ -1,4 +1,27 @@
 package com.mj.wims.controller;
 
+import com.mj.wims.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/messages")
 public class MessageController {
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok().body(messageRepository.findAll());
+    };
 }
