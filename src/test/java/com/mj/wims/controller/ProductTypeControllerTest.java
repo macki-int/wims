878af43929aff920e17019315d5863e69fccc7bf
Marek <macki@int.pl>
2021-10-25 23:16:51 +0200

    move ProductTypeControllerTest class to package controller

--- src/test/java/com/mj/wims/controller/ProductTypeControllerTest.java
+++ src/test/java/com/mj/wims/controller/ProductTypeControllerTest.java
@@ -1,6 +1,5 @@
-package com.mj.wims;
+package com.mj.wims.controller;
 
-import com.mj.wims.controller.ProductTypeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
