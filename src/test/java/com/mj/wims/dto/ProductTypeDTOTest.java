a52677bbd02441900fb1c01f335ed58b13032ea2
Marek <macki@int.pl>
2021-10-26 23:44:41 +0200

    add ProductTypeDTOTest class

---
+++ src/test/java/com/mj/wims/dto/ProductTypeDTOTest.java
@@ -0,0 +1,26 @@
+package com.mj.wims.dto;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ProductTypeDTOTest {
+
+    @Test
+    public void whenProductTypeDTOTestCreate(){
+        //BEFORE
+         ProductTypeDTO productTypeDTO = new ProductTypeDTO();
+
+        //WHEN
+        productTypeDTO.setName("ABC");
+        productTypeDTO.setCalculate(true);
+
+        //THEN
+        assertEquals(productTypeDTO.getName(),"ABC");
+        assertEquals(productTypeDTO.getCalculate(), true);
+        assertTrue(productTypeDTO.getCalculate());
+    }
+}
